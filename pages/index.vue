f5bbb62947e85b94efc565764998832f6cdcf421
Lazuardi Fadhilah <Lazuardifadhillah@gmail.com>
2024-05-27 22:07:21 +0700

    customize scroll bar

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,6 @@
 <template>
   <v-row>
     <v-col cols="8" class="frame-content px-6"> <products></products> </v-col>
-    <v-col cols="4">ORDERS</v-col>
+    <v-col cols="4" class="frame-content white">ORDER</v-col>
   </v-row>
 </template>
-
-<style>
-.frame-content {
-  height: 90vh;
-  overflow-y: auto;
-}
-</style>
