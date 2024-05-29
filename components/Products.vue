409018166dbf25e19026054a038822eb932d0c03
Lazuardi Fadhilah <Lazuardifadhillah@gmail.com>
2024-05-29 20:30:44 +0700

    add category filter

--- components/Products.vue
+++ components/Products.vue
@@ -1,18 +1,38 @@
 <template>
   <v-row>
-    <v-col v-for="(product, index) in products" cols="2">
-      <v-card :title="product.title" :ripple="true">
-        <v-card-actions>
-          <v-img
-            :src="require(`@/assets/images/products/${product.thumbnail}`)"
-          >
-          </v-img>
-        </v-card-actions>
-        <v-card-text align="center" class="product-title">
-          {{ product.title }}
-        </v-card-text>
-      </v-card>
-    </v-col>
+    <v-row>
+      <v-col cols="10"> Search </v-col>
+      <v-col cols="2">
+        <v-menu>
+          <template v-slot:activator="{ on: category }">
+            <v-btn v-on="category" color="primary"> category </v-btn>
+          </template>
+          <v-list>
+            <v-list-item-group>
+              <v-list-item v-for="(category, index) in categories">
+                <v-list-item-title> {{ category.title }}</v-list-item-title>
+              </v-list-item>
+            </v-list-item-group>
+          </v-list>
+        </v-menu>
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col v-for="(product, index) in products" cols="2">
+        <v-card :title="product.title" :ripple="true">
+          <v-card-actions>
+            <v-img
+              :src="require(`@/assets/images/products/${product.thumbnail}`)"
+            >
+            </v-img>
+          </v-card-actions>
+          <v-card-text align="center" class="product-title">
+            {{ product.title }}
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
   </v-row>
 </template>
 
@@ -20,6 +40,20 @@
 export default {
   data() {
     return {
+      categories: [
+        {
+          id: 1,
+          title: 'Smartphone',
+        },
+        {
+          id: 2,
+          title: 'Camera',
+        },
+        {
+          id: 3,
+          title: 'Televisi',
+        },
+      ],
       products: [
         {
           id: 1,
